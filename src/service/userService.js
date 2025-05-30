d03f5447d8be2b9a832883f1c5b8ec6a17bee0b1
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-05-30 15:12:37 +0530

    user service added

---
+++ src/service/userService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:4000/api/v1/users'
+
+export const login = async (email, password) => {
+    const data = {
+        email: email,
+        password: password
+    };
+    const res = await axios.post(`${baseUrl}/login`, data);
+    return res;
+}
+
+
+
+export const sign = async (fname, lname, phone, email, password) => {
+    const data = {
+        fname: fname,
+        lname: lname,
+        phone: phone,
+        email: email,
+        password: password
+    };
+    const res = await axios.post(`${baseUrl}/sign`, data);
+    return res;
+}
