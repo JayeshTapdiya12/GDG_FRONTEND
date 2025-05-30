ac7d7913975644f80537783f4bc9b80d3d24b3b7
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-05-30 15:11:14 +0530

    footer done

---
+++ src/copoments/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Style/footer.css'
+const Footer = () => {
+    return (
+        <>
+            <footer className='sticky-footer'>
+                © 2025 Copyright: Scholarship Portal
+            </footer>
+        </>
+    )
+}
+
+export default Footer
