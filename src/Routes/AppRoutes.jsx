8016255778e50ec22e67feaa45835cd22a37de9b
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-05-30 16:29:21 +0530

    routing and app css updated

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -4,18 +4,23 @@ import Sign from '../pages/Sign';
 import Home from '../pages/Home';
 import { AuthRoute } from './AuthRoute';
 import { ProtectedRoute } from './ProtectedRoute';
-
+import About from '../pages/About'
+import Contact from '../pages/Contact'
+import Form from '../copoments/Form'
 const AppRouter = () => {
     return (
         <>
 
             <BrowserRouter>
                 <Routes>
+
                     <Route path='/' exact element={<AuthRoute><Login /></AuthRoute>} />
 
                     <Route path='/sign' element={<AuthRoute><Sign /></AuthRoute>} />
                     <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
-
+                    <Route path='/about' element={<ProtectedRoute><About /></ProtectedRoute>} />
+                    <Route path='/contact' element={<ProtectedRoute><Contact /></ProtectedRoute>} />
+                    <Route path='/form' element={<ProtectedRoute><Form /></ProtectedRoute>} />
                 </Routes>
             </BrowserRouter>
         </>
