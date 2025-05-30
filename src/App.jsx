59054aa16f5915c700b94795d6605d8d382ba492
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-05-30 15:12:45 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+// import Login from './pages/Login'
+import AppRouter from './Routes/AppRoutes.jsx'
 
 function App() {
 
   return (
     <>
-
+      <AppRouter />
     </>
   )
 }
